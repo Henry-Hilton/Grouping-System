2639c8018657626024864fb24438ed368fef3f3c
Yangsen Halim <yangsenhalim1@outlook.com>
2025-09-28 17:59:53 +0700

    update add_dosen.php
    require_once('../partials/admin_menu.php');

--- admin/add_dosen.php
+++ admin/add_dosen.php
@@ -4,17 +4,18 @@ $required_role = 'admin';
 require_once('../partials/check_session.php');
 require_once('../db_connect.php');
 require_once('../partials/header.php');
+require_once('../partials/admin_menu.php');
 ?>
 
 <div class="container">
   <h1>Add New Lecturer</h1>
-  
+
   <form action="add_dosen_process.php" method="post" enctype="multipart/form-data">
     <div class="form-group">
       <label for="npk">NPK</label>
       <input type="text" id="npk" name="npk" class="form-control" required maxlength="6">
     </div>
-    
+
     <div class="form-group">
       <label for="nama">Name</label>
       <input type="text" id="nama" name="nama" class="form-control" required>
@@ -22,26 +23,26 @@ require_once('../partials/header.php');
 
     <hr>
     <h4>Create Login Account</h4>
-    
+
     <div class="form-group">
       <label for="username">Username</label>
       <input type="text" id="username" name="username" class="form-control" required>
     </div>
-    
+
     <div class="form-group">
       <label for="password">Password</label>
       <input type="password" id="password" name="password" class="form-control" required>
     </div>
-    
+
     <div class="form-group">
       <label for="foto">Photo</label>
       <input type="file" id="foto" name="foto" class="form-control">
     </div>
-    
+
     <button type="submit" class="btn-submit">Add Lecturer</button>
     <a href="manage_dosen.php" class="btn-cancel">Cancel</a>
   </form>
-  
+
 </div>
 
 <?php
